43cd5ec7de60a2d728e52237201c0895f81face7
cmorr22 <cmorr22@uwo.ca>
2017-02-14 23:16:39 -0500

    Made an error in previous commit

--- src/Course.java
+++ src/Course.java
@@ -2,19 +2,12 @@
  * Created by cmorris on 2017-02-14.
  */
 
-import java.util.List;
-
 public class Course {
     String cname;
     String description;
-    List<Course> courseList;
 
     public Course(String cname, String description){
         this.cname=cname;
         this.description=description;
-    }
-
-    public void addCourse(Course c){
-        courseList.add(c);
     }
 }
